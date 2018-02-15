aa7395c4f3fe5c2b1454e237456605f4b1447b48
fillupthe3rd <pca3.92@gmail.com>
2018-02-15 13:03:37 -0600

    MTD comp

--- volume_check.py
+++ volume_check.py
@@ -7,6 +7,16 @@ import numpy as np
 import pandas as pd
 import pyodbc
 from pandas import ExcelWriter
+from datetime import datetime as dt
+import calendar
+
+
+y = dt.now().year
+m = dt.now().month
+d = dt.now().day
+currentMonthID = y*100 + m
+currMonthDays = calendar.monthrange(dt.now().year, dt.now().month)
+mtd = (currMonthDays[1]/d)
 
 # SQL
 conn = pyodbc.connect(r'DRIVER={ODBC Driver 13 for SQL Server};'
@@ -19,13 +29,10 @@ sql = '''
     (
         select dc.ClientParentNameShort
             , dpr.Product
-            , dpr.DimProductKey
-            --, dd.DateDay
             , dd.DateMonthID
             , sum(fc.ClaimCount) Claims
             , sum(fc.CMAllowed) Charges
             
-    
         from FactClaim fc
             join DimDate dd on fc.dimdatereceivedkey = dd.dimdatekey
             join dimclient dc on fc.dimclientkey = dc.dimclientkey
@@ -42,19 +49,15 @@ sql = '''
             dce.ClaimEligible = 'Eligible'
                 and dd.DateDay between (convert(date, getdate() - 120)) and (convert(date, getdate()))
               
-    
         group by 
             dc.ClientParentNameShort
             , dpr.Product
             , dd.DateMonthID
-            , dpr.DimProductKey
-            --, dd.DateDay
         
     )
     
     select c.ClientParentNameShort
         , c.Product
-        , c.DimProductKey
         , c.DateMonthID
         , c.Claims
         , c.Charges
@@ -65,7 +68,6 @@ sql = '''
         --, cast(c.Claims / (lag(c.Claims, 1) over(partition by c.ClientParentNameShort, c.Product order by c.DateMonthID)) as decimal(20,3)) as pdiff_claims
         --, cast(c.Charges / (lag(c.Charges, 1) over(partition by c.ClientParentNameShort, c.Product order by c.DateMonthID)) -1 as decimal(20,3)) as pdiff_charges
         
-    
     from cte_daily c 
     
     order by c.ClientParentNameShort, c.Product, c.DateMonthID
@@ -76,27 +78,32 @@ df = pd.read_sql(sql, conn)
 conn.close()
 
 # Calc and split
-df['pDiff_Claims'] = df['Claims'] / df['Claims_prev'] - 1
-df['pDiff_Charges'] = df['Charges'] / df['Charges_prev'] - 1
+df['Claims_MTD'] = df['Claims']*mtd
+df['Charges_MTD'] = df['Charges']*mtd
+df['pDiff_Claims'] = df['Claims_MTD'] / df['Claims_prev'] - 1
+df['pDiff_Charges'] = df['Charges_MTD'] / df['Charges_prev'] - 1
+df = df[df['DateMonthID'] == currMonthID]
 
 df_flag = df[(df.pDiff_Charges >= .25) | (df.pDiff_Charges <= -.25)]
 df_grp = df_flag.groupby('Product')
 
 df_med = df_grp.get_group('Group Health')
-    df_med = df_med.append(df_grp.get_group('Claim Settlement (PPN)'))
-        df_med = df_med.append(df_grp.get_group('Medicare Pricing Solutions'))
+    df_med = df_med.append(df_grp.get_group('Claim Settlement (PPN)'), ignore_index=True)
+        df_med = df_med.append(df_grp.get_group('Medicare Pricing Solutions'), ignore_index=True)
 
 df_dent = df_grp.get_group('Dental')
 df_wc = df_grp.get_group('Workers Comp')
 
 # Write results to excel
 writer = pd.ExcelWriter(r'C:\Users\pallen\Documents\Volume_Check_test.xlsx')
-df.to_excel(writer, 'Sheet1')
+
+df_flag.to_excel(writer, 'Sheet1')
+df_med.to_excel(writer, 'Medical')
+df_dent.to_excel(writer, 'Dental')
+df_wc.to_excel(writer, 'WC')
 
 writer.save()
 
-
 # check
-
 total = len(df_flag)
 total == len(df_med) + len(df_dent) + len(df_wc)
