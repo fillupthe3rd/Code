d0c64732a52a2fed8186575b574ad2117d441ecf
Philip Allen <Philip.Allen@zelis.com>
2018-02-18 00:17:52 -0600

    init

---
+++ volumeCheckCat.py
@@ -0,0 +1,93 @@
+"""
+Client Volume Monitor
+
+"""
+
+import numpy as np
+import pandas as pd
+from pandas import TimeGrouper
+import pyodbc
+from pandas import ExcelWriter
+from datetime import datetime as dt
+import calendar
+from matplotlib import pyplot as plt
+from functools import lru_cache
+
+y = dt.now().year
+m = dt.now().month
+d = dt.now().day
+currMonthID = y * 100 + m
+currMonthDays = calendar.monthrange(y, m)
+mtd = (currMonthDays[1] / d)
+
+# SQL
+conn = pyodbc.connect(r'DRIVER={ODBC Driver 13 for SQL Server};'
+                      r'SERVER=businteldw.stratose.com,1565;'
+                      r'DATABASE=CAIDataWarehouse;'
+                      r'Trusted_Connection=yes')
+maxID = "13006445"
+sql = '''
+    select dc.ClientParentNameShort
+        , dpr.Product
+        , Grouped = 
+        case dpr.Product
+            when 'Dental' then 'Dental'
+            when 'Workers Comp' then 'Workers Comp'
+            else 'Medical'
+        end
+        , dd.DateMonthID 
+        , dd.DateWeekID
+        , dd.DateDay
+        , dstc.CategoryDesc
+        , sum(fc.ClaimCount) Claims
+        , sum(fc.CMAllowed) Charges
+        , lag(c.Claims, 1) over(partition by c.ClientParentNameShort, c.Product order by c.DateMonthID) claimsLag
+        , lag(c.Charges, 1) over(partition by c.ClientParentNameShort, c.Product order by c.DateMonthID) chargesLag
+
+    from FactClaim fc
+        join DimDate dd on fc.dimdatereceivedkey = dd.dimdatekey
+        join dimclient dc on fc.dimclientkey = dc.dimclientkey
+        join dimclaimeligible dce on fc.dimclaimeligiblekey = dce.dimclaimeligiblekey
+        join dimservicetypecategory dstc on fc.dimservicetypecategorykey = dstc.dimservicetypecategorykey
+        join dimproduct dpr on fc.dimproductkey = dpr.dimproductkey
+        join dimclaimtype dct on fc.dimclaimtypekey = dct.dimclaimtypekey
+
+    where 
+        dce.ClaimEligible = 'Eligible'
+         and dd.DateDay between (convert(date, getdate() - 90)) and (convert(date, getdate()))
+
+    group by 
+        dc.ClientParentNameShort
+        , dpr.Product
+        , dd.DateMonthID
+        , dd.DateWeekID  
+        , dd.DateDay
+        , dstc.CategoryDesc
+;
+'''
+
+df = pd.read_sql(sql, conn)
+conn.close()
+
+# Calc and split
+df['claimsMTD'] = df['Claims'] * mtd
+df['chargesMTD'] = df['Charges'] * mtd
+df['claims%Lag'] = df['claimsMTD'] / df['claimsLag'] - 1
+df['charges%Lag'] = df['chargesMTD'] / df['chargesLag'] - 1
+# df = df[df['DateMonthID'] == currMonthID]
+
+dfFlag = df[(df.['charges%Lag'] >= .25) | (df.['charges%Lag'] <= -.25)]
+
+dfMed = dfFlag[dfFlag['Grouped'] == "Medical"]
+dfDent = dfFlag[dfFlag['Grouped'].Product == "Dental"]
+dfWc = dfFlag[dfFlag['Grouped'] == "Workers Comp"]
+
+# Write results to excel
+writer = pd.ExcelWriter(r'C:\Users\pallen\Documents\Volume_Check_test.xlsx')
+
+dfFlag[dfFlag['Grouped'] == "Medical"].to_excel(writer, 'Sheet1')
+dfFlag[dfFlag['Grouped'] == "Medical"].to_excel(writer, 'Medical')
+dfFlag[dfFlag['Grouped'].Product == "Dental"].to_excel(writer, 'Dental')
+dfFlag[dfFlag['Grouped'] == "Workers Comp"].to_excel(writer, 'WC')
+
+writer.save()
