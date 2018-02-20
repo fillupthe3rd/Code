85d61a9ecf82f3e9c60cf20c8407b4c898785b4c
fillupthe3rd <pca3.92@gmail.com>
2018-02-20 13:05:50 -0600

    edits

--- volumeCheckCat.py
+++ volumeCheckCat.py
@@ -12,7 +12,9 @@ from datetime import datetime as dt
 import calendar
 from matplotlib import pyplot as plt
 from functools import lru_cache
+import xlsxwriter
 
+# Declarations
 y = dt.now().year
 m = dt.now().month
 d = dt.now().day
@@ -25,69 +27,80 @@ conn = pyodbc.connect(r'DRIVER={ODBC Driver 13 for SQL Server};'
                       r'SERVER=businteldw.stratose.com,1565;'
                       r'DATABASE=CAIDataWarehouse;'
                       r'Trusted_Connection=yes')
-maxID = "13006445"
+
 sql = '''
     select dc.ClientParentNameShort
-        , dpr.Product
         , Grouped = 
         case dpr.Product
             when 'Dental' then 'Dental'
-            when 'Workers Comp' then 'Workers Comp'
+            when 'Workers Comp' then 'WC'
             else 'Medical'
         end
+        --, dpr.Product
         , dd.DateMonthID 
-        , dd.DateWeekID
-        , dd.DateDay
-        , dstc.CategoryDesc
         , sum(fc.ClaimCount) Claims
         , sum(fc.CMAllowed) Charges
-        , lag(c.Claims, 1) over(partition by c.ClientParentNameShort, c.Product order by c.DateMonthID) claimsLag
-        , lag(c.Charges, 1) over(partition by c.ClientParentNameShort, c.Product order by c.DateMonthID) chargesLag
-
+            
     from FactClaim fc
         join DimDate dd on fc.dimdatereceivedkey = dd.dimdatekey
         join dimclient dc on fc.dimclientkey = dc.dimclientkey
         join dimclaimeligible dce on fc.dimclaimeligiblekey = dce.dimclaimeligiblekey
+        join dimdiscountmethod ddm on fc.dimdiscountmethodkey = ddm.dimdiscountmethodkey
+        join dimprovider dp on fc.dimproviderkey = dp.dimproviderkey
         join dimservicetypecategory dstc on fc.dimservicetypecategorykey = dstc.dimservicetypecategorykey
+        join dimnetwork dn on fc.dimnetworkkey = dn.dimnetworkkey
         join dimproduct dpr on fc.dimproductkey = dpr.dimproductkey
         join dimclaimtype dct on fc.dimclaimtypekey = dct.dimclaimtypekey
-
+        join DimClaimStatus dcs on fc.DimClaimStatusKey = dcs.DimClaimStatusKey
+    
     where 
         dce.ClaimEligible = 'Eligible'
-         and dd.DateDay between (convert(date, getdate() - 90)) and (convert(date, getdate()))
-
-    group by 
-        dc.ClientParentNameShort
+            and dd.DateDay between (convert(date, getdate() - 70)) and (convert(date, getdate()))
+              
+    group by dc.ClientParentNameShort
         , dpr.Product
         , dd.DateMonthID
-        , dd.DateWeekID  
-        , dd.DateDay
-        , dstc.CategoryDesc
-;
+    
+    order by dc.ClientParentNameShort
+        , dpr.Product
+        , dd.DateMonthID
+
 '''
 
-df = pd.read_sql(sql, conn)
+df1 = pd.read_sql(sql, conn)
 conn.close()
 
-# Calc and split
+# Calcs
+df = df1.set_index(['ClientParentNameShort', 'Grouped', 'DateMonthID'])
 df['claimsMTD'] = df['Claims'] * mtd
 df['chargesMTD'] = df['Charges'] * mtd
-df['claims%Lag'] = df['claimsMTD'] / df['claimsLag'] - 1
-df['charges%Lag'] = df['chargesMTD'] / df['chargesLag'] - 1
-# df = df[df['DateMonthID'] == currMonthID]
+df['lagClaim'] = df['Claims'].shift(1)
+df['lagCharge'] = df['Charges'].shift(1)
+df['diffClaim'] = df['claimsMTD'] - df['lagClaim']
+df['diffCharge'] = df['chargesMTD'] - df['lagCharge']
+df.dropna(inplace=True)
+df.reset_index(inplace=True)
+df = df[df['DateMonthID'] == currMonthID]
+df['claims%Lag'] = df['claimsMTD'] / df['lagClaim'] - 1
+df['charges%Lag'] = df['chargesMTD'] / df['lagCharge'] - 1
 
-dfFlag = df[(df.['charges%Lag'] >= .25) | (df.['charges%Lag'] <= -.25)]
+dfFlag = df[(df['charges%Lag'] >= .25) | (df['charges%Lag'] <= -.25)]
 
-dfMed = dfFlag[dfFlag['Grouped'] == "Medical"]
-dfDent = dfFlag[dfFlag['Grouped'].Product == "Dental"]
-dfWc = dfFlag[dfFlag['Grouped'] == "Workers Comp"]
+dfMed = dfFlag[dfFlag.Grouped == "Medical"]
+dfDent = dfFlag[dfFlag.Grouped == "Dental"]
+dfWC = dfFlag[dfFlag.Grouped == "WC"]
 
 # Write results to excel
-writer = pd.ExcelWriter(r'C:\Users\pallen\Documents\Volume_Check_test.xlsx')
+writer = pd.ExcelWriter(r'C:\Users\pallen\Documents\Volume_Check_test.xlsx', engine='xlsxwriter')
 
-dfFlag[dfFlag['Grouped'] == "Medical"].to_excel(writer, 'Sheet1')
-dfFlag[dfFlag['Grouped'] == "Medical"].to_excel(writer, 'Medical')
-dfFlag[dfFlag['Grouped'].Product == "Dental"].to_excel(writer, 'Dental')
-dfFlag[dfFlag['Grouped'] == "Workers Comp"].to_excel(writer, 'WC')
+dfMed.to_excel(writer,'Medical', startrow=1, startcol=1, index=False)
+dfDent.to_excel(writer, 'Dental', startrow=1, startcol=1, index=False)
+dfWC.to_excel(writer, 'WC', startrow=1, startcol=1, index=False)
 
+writer.save()
+
+workbook = writer.book
+worksheet = workbook.sheet['Medical']
+
+worksheet.set_zoom(90)
 writer.save()
